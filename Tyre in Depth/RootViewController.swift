358af98526e3fe7d3623c7c866e611c23b6f76ea
Tommy <wei.neng.loh@conti.my>
2020-01-12 09:36:29 +0800

    Initial Commit

---
+++ Tyre in Depth/RootViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootViewController.swift
+//  Tyre in Depth
+//
+//  Created by Loh, Wei Neng (uic24998) on 12/01/2020.
+//  Copyright © 2020 Loh, Wei Neng (uic24998). All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
